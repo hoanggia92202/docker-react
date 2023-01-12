30d24c120246d1711efd69a910aa7da05f1e3b83
duc le <ducle@ducs-MacBook-Pro.local>
2023-01-12 02:57:20 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
         <a
-          className="App-link"
+          className="App-link text-warning"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          learn react 3
         </a>
       </header>
     </div>
